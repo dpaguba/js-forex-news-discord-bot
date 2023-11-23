2277406e420ce3b4d8f8fecd968ea29c627a0758
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-23 12:19:42 +0100

    feat: load the command files on stratup

--- index.js
+++ index.js
@@ -28,3 +28,18 @@ for (const file of eventFiles) {
         client.on(event.name, (...args) => event.execute(...args))
     }
 }
+
+client.commands = new Collection()
+const commandsPath = path.join(__dirname, "commands")
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"))
+
+for (const file of commandFiles) {
+    const filePath = path.join(eventsPath, file)
+    const command = require(filePath)
+
+    if ("data" in command && "execute" in command) {
+        client.commands.set(command.data.name, command)
+    } else {
+        console.log(`[WARNING]: The command at ${filePath} is missing a required "data" or "execute" property`);
+    }
+}
