3903accd8766580f532827b1f8e59c1060c3d51d
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-23 19:32:13 +0100

    feat: create server request

--- server-request.js
+++ server-request.js
@@ -1,27 +1,35 @@
-const http = require('https');
+const request = require('request');
 
 const options = {
     method: 'GET',
-    hostname: 'forex-news-alerts.p.rapidapi.com',
-    port: null,
-    path: '/api/v1/ForexArticleDetails/Req123456/bf4dfb95-cef1-492c-948e-b5023a9c23eb:AUDUSD-20201015',
+    url: 'https://forex-news-alerts.p.rapidapi.com/api/v1/ForexArticleDetails/Req123456/bf4dfb95-cef1-492c-948e-b5023a9c23eb:EURUSD-20231123',
     headers: {
         'X-RapidAPI-Key': '62256a3dc6msheeb4458a2735758p13b70bjsncc100bed6959',
         'X-RapidAPI-Host': 'forex-news-alerts.p.rapidapi.com'
     }
 };
 
-const req = http.request(options, function (res) {
-    const chunks = [];
+request(options, function (error, response, body) {
+    if (error) throw new Error(error);
 
-    res.on('data', function (chunk) {
-        chunks.push(chunk);
-    });
+    try {
+        const { result } = JSON.parse(body);
 
-    res.on('end', function () {
-        const body = Buffer.concat(chunks);
-        console.log(body.toString());
-    });
+        console.log(response);
+
+        // if (newsList && newsList.length > 0) {
+        //     console.log('List of News Articles:');
+        //     newsList.forEach(article => {
+        //         console.log(`- Title: ${article.title}`);
+        //         console.log(`  Description: ${article.description}`);
+        //         console.log(`  URL: ${article.url}`);
+        //         console.log('------------------------');
+        //     }
+        //     );
+        // } else {
+        //     console.log('No news articles found.');
+        // }
+    } catch (error) {
+        console.error('Error parsing JSON:', error);
+    }
 });
-
-req.end();
