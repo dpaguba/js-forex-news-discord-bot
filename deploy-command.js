d96ea3095e52ddff2fceaed1ca7eb5da92723e30
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-23 12:56:58 +0100

    feat: output for each command's data

--- deploy-command.js
+++ deploy-command.js
@@ -10,8 +10,13 @@ const fs = require("node:fs");
 console.log(token);
 
 const commands = [];
-// Grab all the command files from the commands directory you created earlier
 const commandFiles = fs
     .readdirSync("./commands")
     .filter((file) => file.endsWith(".js"));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
 
