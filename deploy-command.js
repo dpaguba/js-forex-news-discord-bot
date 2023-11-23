9a0cfd154613a5cd50ef8ec1b374443c3bc0604f
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-23 12:58:36 +0100

    feat: deploy commands

--- deploy-command.js
+++ deploy-command.js
@@ -19,6 +19,25 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-// Construct and prepare an instance of the REST module
 const rest = new REST({ version: "10" }).setToken(token);
+
+(async () => {
+    try {
+        console.log(
+            `Started refreshing ${commands.length} application (/) commands.`
+        );
+
+        // fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands }
+        );
+
+        console.log(
+            `Successfully reloaded ${data.length} application (/) commands.`
+        );
+    } catch (error) {
+        console.error(error);
+    }
+})();
 
