4707ffd184173660f3812e71c54af7ffbd6ea6e0
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-23 12:57:48 +0100

    feat: construct instance fo the REST module

--- deploy-command.js
+++ deploy-command.js
@@ -14,9 +14,11 @@ const commandFiles = fs
     .readdirSync("./commands")
     .filter((file) => file.endsWith(".js"));
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.push(command.data.toJSON());
 }
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: "10" }).setToken(token);
 
