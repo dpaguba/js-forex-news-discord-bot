5cd50a84f10033511f72f45ba5b581028b6c2be3
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-24 13:24:03 +0100

    feat: parse the accumulated data as JSON and write to a file

--- server-request.js
+++ server-request.js
@@ -17,6 +17,36 @@ const options = {
 
 let responseData = '';
 
+const req = https.request(options, (res) => {
+    res.on('data', (chunk) => {
+        responseData += chunk;
+    });
+
+    res.on('end', () => {
+        const jsonData = JSON.parse(responseData).filter(
+            function (data) {
+
+                delete data.country
+                delete data.change
+                delete data.changePercentage
+                delete data.actual
+
+                // maybe its bug, but i need to put utc+2 (im in utc+1), so this works properly
+                const options = { timeZone: "Europe/Kyiv", hour: "2-digit", minute: "2-digit" };
+                data.date = new Date(data.date).toLocaleTimeString("en-GB", options) + " CET"
+
+                return (data.currency === "EUR" || data.currency === "GBP" || data.currency === "USD") && (data.impact === "Medium" || data.impact === "High")
+            }
+        );
+        fs.writeFileSync('commands/economic_calendar.json', JSON.stringify(jsonData, null, 2));
+    });
+});
+
+req.on('error', (error) => {
+    console.error(error);
+});
+
+req.end();
 
 
 function formatDate(date) {
