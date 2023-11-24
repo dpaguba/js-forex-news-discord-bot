51526e0c328d0f5e7b36bf7e5b5514155eb51282
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-24 13:24:27 +0100

    fix: del unnecessary require

--- server-request.js
+++ server-request.js
@@ -1,8 +1,6 @@
 require("dotenv").config()
 const https = require('https');
 const fs = require('fs');
-const { log } = require("console");
-const { time } = require("discord.js");
 
 const { APIKEY: apikey } = process.env
 
