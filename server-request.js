5e28bf0e45175317934c6b43d5f10a7ca03c3fb0
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-24 13:20:20 +0100

    feat: create request template

--- server-request.js
+++ server-request.js
@@ -6,3 +6,12 @@ const { time } = require("discord.js");
 
 const { APIKEY: apikey } = process.env
 
+const currentDate = new Date().toDateString();
+
+const options = {
+    hostname: 'financialmodelingprep.com',
+    port: 443,
+    path: `/api/v3/economic_calendar?from=${formatDate(currentDate)}&to=${formatDate(currentDate)}&apikey=${apikey}`,
+    method: 'GET'
+}
+
