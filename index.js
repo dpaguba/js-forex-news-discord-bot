d96c3b9992c8cb8e554d4461e97192650ba25556
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-24 15:34:18 +0100

    feat: add comments for better readability

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("node:path")
 const { fetchDataAndWriteToFile } = require('./fetchData')
 
 const { DISCORD_TOKEN: token } = process.env
+
 const { Client, GatewayIntentBits, Collection } = require("discord.js")
 
 const client = new Client({
@@ -18,6 +19,7 @@ const client = new Client({
 const eventsPath = path.join(__dirname, "events")
 const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith(".js"))
 
+// Event Handling Logic
 for (const file of eventFiles) {
     const filePath = path.join(eventsPath, file)
     const event = require(filePath)
@@ -33,6 +35,7 @@ client.commands = new Collection()
 const commandsPath = path.join(__dirname, "commands")
 const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"))
 
+// Command Handling Logic
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file)
     const command = require(filePath)
